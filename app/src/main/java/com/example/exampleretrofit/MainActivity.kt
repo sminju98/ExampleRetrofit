067d729e61ae8bc9b67ea9cc2e23aceffc3b4f64
Parkduksung <duksung1234@naver.com>
2021-10-12 22:45:22 +0900

    [feature] 로그를 찍으면서 결과값이 나오는지 확인.

--- app/src/main/java/com/example/exampleretrofit/MainActivity.kt
+++ app/src/main/java/com/example/exampleretrofit/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.example.exampleretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.exampleretrofit.retrofit.WeatherResponse
 import com.example.exampleretrofit.retrofit.WeatherRetrofit
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getCurrentWeather(707860)
+
     }
 
     private fun getCurrentWeather(id: Int) {
-        WeatherRetrofit.createWeatherApi().getCurrentWeather(cityId = id)
-            .enqueue(object : Callback<WeatherResponse>{
-                override fun onResponse(p0: Call<WeatherResponse>, p1: Response<WeatherResponse>) {
+        WeatherRetrofit.createWeatherApi().getCurrentWeather(id = id)
+            .enqueue(object : Callback<WeatherResponse> {
+                override fun onResponse(
+                    p0: Call<WeatherResponse>,
+                    response: Response<WeatherResponse>
+                ) {
 
+                    response.body()?.let {
+                        Log.d("결과", it.name)
+                    }
                 }
 
                 override fun onFailure(p0: Call<WeatherResponse>, p1: Throwable) {
-
+                    Log.d("결과", p1.message ?: "에러")
                 }
             })
 
